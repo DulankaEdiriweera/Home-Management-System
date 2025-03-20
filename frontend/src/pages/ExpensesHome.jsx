eec169bac716a7656713a5df0e2806d1e428ccff
himashi17 <160330515+himashi17@users.noreply.github.com>
2025-03-20 11:25:06 +0530

    crud function finished

---
+++ frontend/src/pages/ExpensesHome.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import ExpenseSidebar from '../components/ExpensesSideBar';
+
+const ExpensesHome = () => {
+    return (
+        <div className="flex min-h-screen bg-gray-100">
+            {/* Sidebar */}
+            <ExpenseSidebar />
+
+            {/* Main Content */}
+            <div className="flex-1 p-6">
+                <h1 className="text-3xl font-semibold mb-4">Expenses Dashboard</h1>
+                <p>Total Expenses</p>
+                <p>Expenses Overview</p>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesHome;

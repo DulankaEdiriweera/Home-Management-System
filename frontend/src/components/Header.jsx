9a257465204db60ce48c2e908bb6431a68f371fc
DinushaRanasinghe03 <dinushaumayangani02@gmail.com>
2025-03-20 12:08:07 +0530

    commit changes in Header.jsx , ExpensesHome and TaskPage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center py-5 px-8 bg-white shadow-md">
-
+      {/* Logo */}
       <div className="flex items-center">
-
         <div className="mr-4">
-
-          <img
-            src="/src/assets/HOME TRACK LOGO.png"
-            alt="Home Track Logo"
-            width="100"
-            height="100"
-          />
-
+          <Link to="/">
+            <img
+              src="/src/assets/HOME TRACK LOGO.png"
+              alt="Home Track Logo"
+              width="100"
+              height="100"
+              className="cursor-pointer"
+            />
+          </Link>
         </div>
       </div>
 
       {/* Navigation */}
       <nav>
         <ul className="flex space-x-20">
-          <li className="font-medium hover:text-blue-500 cursor-pointer transition-colors">INVENTORY</li>
-          <li className="font-medium hover:text-blue-500 cursor-pointer transition-colors">TASK MASTER</li>
-          <li className="font-medium hover:text-blue-500 cursor-pointer transition-colors">EXPENSES TRACKER</li>
-          <li className="font-medium hover:text-blue-500 cursor-pointer transition-colors">SHOPPING LIST</li>
+          <li>
+            <Link to="/inventoryHome" className="font-medium hover:text-blue-500 cursor-pointer transition-colors">
+              INVENTORY
+            </Link>
+          </li>
+          <li>
+            <Link to="/tasks" className="font-medium hover:text-blue-500 cursor-pointer transition-colors">
+              TASK MASTER
+            </Link>
+          </li>
+          <li>
+            <Link to="/expenses" className="font-medium hover:text-blue-500 cursor-pointer transition-colors">
+              EXPENSES TRACKER
+            </Link>
+          </li>
+          <li>
+            <Link to="/shopping-list" className="font-medium hover:text-blue-500 cursor-pointer transition-colors">
+              SHOPPING LIST
+            </Link>
+          </li>
         </ul>
       </nav>
 
